09ab0ebff2ad3d6ea08dd019ceb0438e9b44cf9e
gitubanana <ptyolo666@gmail.com>
2024-12-02 20:03:41 +0900

    feat(Problem2): 문제2 기능

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -2,7 +2,35 @@ package onboarding;
 
 public class Problem2 {
     public static String solution(String cryptogram) {
-        String answer = "answer";
-        return answer;
+        StringBuilder result = new StringBuilder(cryptogram.length());
+
+        for (int i = 0; i < cryptogram.length(); i++) {
+            int resultLength = result.length();
+            char toAppend = cryptogram.charAt(i);
+
+            if (resultLength == 0) {
+                result.append(toAppend);
+                continue;
+            }
+
+            char lastCh = result.charAt(resultLength - 1);
+            if (lastCh == toAppend) {
+                result.delete(resultLength - 1, resultLength);
+                i = moveTillConsecutiveLastCharacter(i, lastCh, cryptogram);
+                continue;
+            }
+
+            result.append(toAppend);
+        }
+        return result.toString();
+    }
+
+    private static int moveTillConsecutiveLastCharacter(int i, char lastCh, String cryptogram) {
+        for (; i < cryptogram.length(); i++) {
+            if (cryptogram.charAt(i) != lastCh) {
+                break;
+            }
+        }
+        return i - 1;
     }
 }
