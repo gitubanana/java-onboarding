61095d9c87be51d1674d0f282ba3d53e3fe0d7ef
gitubanana <ptyolo666@gmail.com>
2024-12-02 20:59:07 +0900

    feat(Problem5): 문제5 기능

--- src/main/java/onboarding/Problem5.java
+++ src/main/java/onboarding/Problem5.java
@@ -1,11 +1,20 @@
 package onboarding;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Problem5 {
+    private static final List<Integer> UNITS = List.of(50_000, 10_000, 5_000, 500, 1_000, 100, 50, 10, 1);
+
     public static List<Integer> solution(int money) {
-        List<Integer> answer = Collections.emptyList();
+        List<Integer> answer = new ArrayList<>(UNITS.size());
+
+        for (int unit : UNITS) {
+            int unitCount = money / unit;
+
+            answer.add(unitCount);
+            money -= unit * unitCount;
+        }
         return answer;
     }
 }
