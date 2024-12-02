cd0fdbdfda447d0cf6b439cb8deae96e72f9a60b
gitubanana <ptyolo666@gmail.com>
2024-12-02 20:12:17 +0900

    feat(Problem3): 문제3 기능

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,29 @@
 package onboarding;
 
 public class Problem3 {
-    public static int solution(int number) {
+    public static int solution(int endNumber) {
         int answer = 0;
+
+        for (int checkNumber = 1; checkNumber <= endNumber; checkNumber++) {
+            answer += count369(checkNumber);
+        }
         return answer;
+    }
+
+    private static int count369(int number) {
+        int count = 0;
+
+        while (number > 0) {
+            int digit = number % 10;
+            if (is369(digit)) {
+                count++;
+            }
+            number /= 10;
+        }
+        return count;
+    }
+
+    private static boolean is369(int number) {
+        return number == 3 || number == 6 || number == 9;
     }
 }
