d033a1b4cb03de7ad930474b942cb23d964b0973
gitubanana <ptyolo666@gmail.com>
2024-12-02 20:42:06 +0900

    feat(Problem4): 문제4 기능

--- src/main/java/onboarding/Problem4.java
+++ src/main/java/onboarding/Problem4.java
@@ -1,8 +1,27 @@
 package onboarding;
 
 public class Problem4 {
+    private static final String LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+    private static final String UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     public static String solution(String word) {
-        String answer = "";
-        return answer;
+        StringBuilder answer = new StringBuilder(word.length());
+
+        for (int i = 0; i < word.length(); i++) {
+            answer.append(convertAlphabet(word.charAt(i)));
+        }
+        return answer.toString();
+    }
+
+    private static char convertAlphabet(char ch) {
+        if (Character.isLowerCase(ch)) {
+            return LOWERCASE.charAt(LOWERCASE.length() - (ch - 'a') - 1);
+        }
+
+        if (Character.isUpperCase(ch)) {
+            return UPPERCASE.charAt(UPPERCASE.length() - (ch - 'A') - 1);
+        }
+
+        return ch;
     }
 }
